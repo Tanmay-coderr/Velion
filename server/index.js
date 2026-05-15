44ef40be0e1ce39974d3557ab7a41af3a2f19edf
Tanmay <tanmayghosh4597@gmail.com>
2026-05-15 17:33:23 +0530

    fixed cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ const app =express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors());
+app.use(cors({
+    origin:"https://velion-psi.vercel.app",
+    credentials:true
+}
+));
 
 connectDb();
 app.use('/api/auth',routes);
